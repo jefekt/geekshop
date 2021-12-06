e3d3b62198145e631ec890d080a71570b1b052f6
jefekt <jefekt@gmail.com>
2021-12-06 23:48:54 +0300

    Update

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,13 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mainapp.views import main, products, contact
+from mainapp.views import index, products, contact, context
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main),
-    path('products', products),
-    path('contact', contact),
+    path('', index, name = "index"),
+    path('products/', products, name = "products"),
+    path('contact/', contact, name = "contact"),
+    path('context/', context),
+    path('product_all', products, name ="product_all"),
+    path('product_home', products, name ="product_home"),
+    path('product_office', products, name ="product_office"),
+    path('product_modern', products, name ="product_modern"),
+    path('product_classic', products, name ="product_classic"),
+    path('product_popular', index, name ="product_popular"),
+    path('product_new', index, name ="product_new"),
 
 
 ]
